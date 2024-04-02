a750a8cfaf6fa2b896f16349e8a08fab4e0eae8a
DaBrogrammer <adnan.9821@gmail.com>
2024-04-01 22:15:38 -0400

    Working Models

--- Assignment2.py
+++ Assignment2.py
@@ -1,39 +1,38 @@
-import torch
-import numpy as np
-from torch.utils.data import Dataset
 from torch import optim, nn
-import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torch.utils.data.dataset import Dataset
-import os
-import PIL.Image as Image
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.data as td
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 from torchvision import transforms
 import os
-
 from torch.utils.data import Dataset
 import torch.nn.functional as F
 import torch
 import PIL.Image as Image
-import numpy as np
 import pandas as pd
-
-
+from sklearn.model_selection import train_test_split
 
 class Pclass(Dataset):
     def __init__(self, mode):
         csv_file = 'Labeled Data.csv'
-
         self.mode = mode
-        self.data_frame = pd.read_csv(csv_file)  # Load CSV file
+        data_frame = pd.read_csv(csv_file)  # Load CSV file
+
+        # Splitting the data into train and test sets
+        train_df, test_df = train_test_split(data_frame, test_size=0.15, random_state=42)
+
+        # Select the appropriate subset based on mode
+        if mode == 'train':
+            self.data_frame = train_df
+        elif mode == 'test':
+            self.data_frame = test_df
+        else:
+            raise ValueError("Mode must be 'train' or 'test'")
+
         self.image_paths = self.data_frame['path'].tolist()
         self.labels = self.data_frame['label'].tolist()
+
+        # Define your transformations here
         self.transform = transforms.Compose([
+            transforms.Resize((96, 96)),  # Resize the image to 96x96
             transforms.ToTensor(),
         ])
 
@@ -42,37 +41,60 @@ class Pclass(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(os.getcwd(), self.image_paths[idx])  # Construct full image path
-        img = Image.open(img_path).convert('RGB')  # Open image
+        img = Image.open(img_path).convert('RGB')  # Open image and convert to RGB
         img = self.transform(img)  # Apply transformations
-        label = self.labels[idx]  # Get label
-        return img, label
 
+        # Convert labels to numerical format
+        label_map = {'Neutral': 0, 'Surprised': 1, 'Happy': 2, 'Focused': 3}
+        label = label_map[self.labels[idx]]  # Convert label from string to integer
+
+        return img, label
 
 
 class MultiLayerFCNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
 
-        self.layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1, stride=1)
+        k_size = 3
+
+        self.layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=k_size, padding=1, stride=1)
         self.B1 = nn.BatchNorm2d(32)
-        self.layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1)
+
+        self.layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=k_size, padding=1, stride=1)
         self.B2 = nn.BatchNorm2d(32)
+
         self.Maxpool = nn.MaxPool2d(2)
-        self.layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, stride=1)
+
+        self.layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=k_size, padding=1, stride=1)
         self.B3 = nn.BatchNorm2d(64)
-        self.layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=1)
+
+        self.layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=k_size, padding=1, stride=1)
         self.B4 = nn.BatchNorm2d(64)
 
-        self.layer5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=1)
+        self.layer5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=k_size, padding=1, stride=1)
         self.B5 = nn.BatchNorm2d(128)
 
-        self.layer6 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1)
-        self.B6 = nn.BatchNorm2d(256)
+        self.layer6 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=k_size, padding=1, stride=1)
+        self.B6 = nn.BatchNorm2d(128)
 
-        self.fc = nn.Linear(256 * 6 * 6, 4)
+        self.layer7 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=k_size, padding=1, stride=1)
+        self.B7 = nn.BatchNorm2d(256)
+
+        self.layer8 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=k_size, padding=1, stride=1)
+        self.B8 = nn.BatchNorm2d(256)
+
+        self.layer9 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=k_size, padding=1, stride=1)
+        self.B9 = nn.BatchNorm2d(512)
+
+        self.layer10 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=k_size, padding=1, stride=1)
+        self.B10 = nn.BatchNorm2d(512)
+
+        self.layer11 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=k_size, padding=1, stride=1)
+        self.B11 = nn.BatchNorm2d(1024)
+
+        self.fc = nn.Linear(512*3*3, 4)
 
     def forward(self, x):
-
         x = self.B1(F.leaky_relu(self.layer1(x)))
         x = self.Maxpool(F.leaky_relu(self.layer2(x)))
         x = self.B2(x)
@@ -81,18 +103,21 @@ class MultiLayerFCNet(nn.Module):
 
         x = self.B5((F.leaky_relu(self.layer5(x))))
         x = self.B6(self.Maxpool(F.leaky_relu(self.layer6(x))))
+        x = self.B7(F.leaky_relu(self.layer7(x)))
+        x = self.B8(self.Maxpool(F.leaky_relu(self.layer8(x))))
+        x = self.B9((F.leaky_relu(self.layer9(x))))
+
+        #print(x.size())
 
         return self.fc(x.view(x.size(0), -1))
 
 
 if __name__ == '__main__':
 
-    batch_size = 400
-    test_batch_size = 100
-    input_size = 3 * 96 * 96  # 3 channels, 32x32 image size
+    batch_size = 32
+    input_size = 3 * 96 * 96  # 3 channels, 96x96 image size
     hidden_size = 50  # Number of hidden units
     output_size = 4  # Number of output classes
-    num_epochs = 10
 
     mode = 'train'
     csv_file = 'Labeled Data.csv'
@@ -106,24 +131,21 @@ if __name__ == '__main__':
     testset = Pclass('test')
     testloader = DataLoader(testset, batch_size, shuffle=True, num_workers=8, drop_last=True)
 
-    epochs = 5
+    epochs = 50
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = MultiLayerFCNet(input_size, hidden_size, output_size)
     model = nn.DataParallel(model)
     model.to(device)
-    # model.load_state_dict(torch.load('path'))
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     BestACC = 0
     for epoch in range(epochs):
         running_loss = 0
         for instances, labels in Trainloader:
             optimizer.zero_grad()
 
-            label_map = {'Neutral': 0, 'Surprised': 1, 'Happy': 2, 'Focused': 3}
-            numerical_labels = [label_map[label] for label in labels]
-            tensor_labels = torch.tensor(numerical_labels).cuda()
+            tensor_labels = torch.tensor(labels).cuda()
 
             output = model(instances.cuda())
             loss = criterion(output, tensor_labels)
@@ -135,20 +157,24 @@ if __name__ == '__main__':
         print(running_loss / len(Trainloader))
 
         model.eval()
+        total = 0
+        correct = 0
         with torch.no_grad():
-            allsamps = 0
-            rightPred = 0
-
             for instances, labels in testloader:
-                output = model(instances.cuda())
-                predictedClass = torch.max(output, 1)
-                allsamps += output.size(0)
-                rightPred += (torch.max(output, 1)[1] == tensor_labels).sum()
+                instances, labels = instances.to(device), labels.to(device)
+                output = model(instances)
+                _, predicted = torch.max(output.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-            ACC = rightPred / allsamps
-            print('Accuracy is=', ACC * 100)
-            if ACC > BestACC:
-                BestACC = ACC
-                # torch.save(model.state_dict())
-                # torch.save(model.state_dict(), 'path')
+        accuracy = 100 * correct / total
+        print(f'Accuracy on the test set: {accuracy:.2f}%')
+
+        if accuracy > BestACC:
+            BestACC = accuracy
+
+            torch.save(model.state_dict(), 'C:/Users/saaba/Desktop/model_epoc50_k3_11Layer.pt')
+
+        print(f'Best accuracy on the test set: {BestACC:.2f}%')
+
         model.train()
